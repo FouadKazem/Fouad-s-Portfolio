2c3b952092452c0e1b1c4bc9e31dc99dc6952dc7
Fouad <kazemfouad53@gmail.com>
2022-10-02 19:59:33 +0400

    Added two logos to the themes button and edit the styling of the header and the themes button

--- js/app.js
+++ js/app.js
@@ -1,25 +1,39 @@
 const styleSheet = document.styleSheets[0]
 const themesBtnHtml = document.querySelector(".themes-btn-slide")
 let themesBtnStyle = null
+let themesBtnBorderStyle = null
 for(let i = 0; i < styleSheet.cssRules.length; i++)
 {
     if(styleSheet.cssRules[i].selectorText === '.themes-btn-slide')
     {
         themesBtnStyle = styleSheet.cssRules[i]
     }
+    else if(styleSheet.cssRules[i].selectorText === '.themes-btn')
+    {
+        themesBtnBorderStyle = styleSheet.cssRules[i]
+    }
 }
-console.log(themesBtnStyle)
+//console.log(themesBtnStyle)
 let themesBtnState = localStorage.getItem('themesBtnState')
 if(themesBtnState === null)
 {
     themesBtnState = 'left'
     localStorage.setItem('themesBtnState', 'left')
+    themesBtnStyle.style.setProperty('background-image', 'url("../img/Sun-logo.png")')
 }
 else
 {
     if(themesBtnState === 'right')
     {
         themesBtnStyle.style.setProperty('transform', 'translateX(2rem)')
+        themesBtnStyle.style.setProperty('background-image', 'url("../img/Moon-logo.png")')
+        themesBtnBorderStyle.style.setProperty('border', '0.25rem solid white')
+        themesBtnBorderStyle.style.setProperty('background-color', 'grey')
+    }
+    else
+    {
+        themesBtnStyle.style.setProperty('transform', 'none')
+        themesBtnStyle.style.setProperty('background-image', 'url("../img/Sun-logo.png")')
     }
 }
 
@@ -29,6 +43,9 @@ themesBtnHtml.addEventListener('click', function() {
         themesBtnState = 'right'
         localStorage.setItem('themesBtnState', 'right')
         themesBtnStyle.style.setProperty('transform', 'translateX(2rem)')
+        themesBtnStyle.style.setProperty('background-image', 'url("../img/Moon-logo.png")')
+        themesBtnBorderStyle.style.setProperty('border', '0.25rem solid white')
+        themesBtnBorderStyle.style.setProperty('background-color', 'grey')
         /*
 
         Insert Code to change styling to dark theme
@@ -40,6 +57,9 @@ themesBtnHtml.addEventListener('click', function() {
         themesBtnState = 'left'
         localStorage.setItem('themesBtnState', 'left')
         themesBtnStyle.style.setProperty('transform', 'none')
+        themesBtnStyle.style.setProperty('background-image', 'url("../img/Sun-logo.png")')
+        themesBtnBorderStyle.style.setProperty('border', '0.25rem solid rgb(50, 50, 50)')
+        themesBtnBorderStyle.style.setProperty('background-color', 'white')
         /*
 
         Insert Code to change styling to light theme
