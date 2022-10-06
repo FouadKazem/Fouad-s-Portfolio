983df99d53748625b1c7bc677c11542a4bcf506c
Fouad <kazemfouad53@gmail.com>
2022-10-07 03:46:46 +0400

    Modified index.html, about-me.html, projects.html, stylesheet.css and app.js
    Edited the desktop header styling, fix some styling bugs in the style sheet, orginize the app.js code (make two functions 'lightTheme' and 'darkTheme' to switch between light theme and dark theme and adding style sheet rules inside hash map to optimize the speed of accessing the css rules

--- js/app.js
+++ js/app.js
@@ -1,69 +1,64 @@
 const styleSheet = document.styleSheets[0]
 const themesBtnHtml = document.querySelector(".themes-btn-slide")
+let cssRules = new Map()
 let themesBtnStyle = null
 let themesBtnBorderStyle = null
-for(let i = 0; i < styleSheet.cssRules.length; i++)
-{
-    if(styleSheet.cssRules[i].selectorText === '.themes-btn-slide')
-    {
-        themesBtnStyle = styleSheet.cssRules[i]
-    }
-    else if(styleSheet.cssRules[i].selectorText === '.themes-btn')
-    {
-        themesBtnBorderStyle = styleSheet.cssRules[i]
-    }
-}
-//console.log(themesBtnStyle)
 let themesBtnState = localStorage.getItem('themesBtnState')
-if(themesBtnState === null)
-{
+for (let i = 0; i < styleSheet.cssRules.length; i++) {
+    cssRules.set(styleSheet.cssRules[i].selectorText, styleSheet.cssRules[i])
+}
+themesBtnStyle = cssRules.get('.themes-btn-slide')
+themesBtnBorderStyle = cssRules.get('.themes-btn')
+
+function lightTheme(state) {
     themesBtnState = 'left'
     localStorage.setItem('themesBtnState', 'left')
+    themesBtnStyle.style.setProperty('transform', 'none')
     themesBtnStyle.style.setProperty('background-image', 'url("../img/Sun-logo.png")')
-}
-else
-{
-    if(themesBtnState === 'right')
-    {
-        themesBtnStyle.style.setProperty('transform', 'translateX(2rem)')
-        themesBtnStyle.style.setProperty('background-image', 'url("../img/Moon-logo.png")')
-        themesBtnBorderStyle.style.setProperty('border', '0.25rem solid white')
-        themesBtnBorderStyle.style.setProperty('background-color', 'grey')
-    }
-    else
-    {
-        themesBtnStyle.style.setProperty('transform', 'none')
-        themesBtnStyle.style.setProperty('background-image', 'url("../img/Sun-logo.png")')
+    themesBtnBorderStyle.style.setProperty('border', '0.25rem solid rgb(50, 50, 50)')
+    themesBtnBorderStyle.style.setProperty('background-color', 'white')
+    cssRules.get('.desktop-header').style.setProperty('background-color', 'rgb(0, 128, 255)')
+    if (state === 'change') {
+        cssRules.get('.desktop-header').style.setProperty('transition', 'background-color 0.5s ease-in-out')
+        setTimeout(function () {
+            cssRules.get('.desktop-header').style.setProperty('transition', 'none')
+        }, 500)
     }
 }
 
-themesBtnHtml.addEventListener('click', function() {
-    if(themesBtnState === 'left')
-    {
-        themesBtnState = 'right'
-        localStorage.setItem('themesBtnState', 'right')
-        themesBtnStyle.style.setProperty('transform', 'translateX(2rem)')
-        themesBtnStyle.style.setProperty('background-image', 'url("../img/Moon-logo.png")')
-        themesBtnBorderStyle.style.setProperty('border', '0.25rem solid white')
-        themesBtnBorderStyle.style.setProperty('background-color', 'grey')
-        /*
-
-        Insert Code to change styling to dark theme
-
-        */
+function darkTheme(state) {
+    themesBtnState = 'right'
+    localStorage.setItem('themesBtnState', 'right')
+    themesBtnStyle.style.setProperty('transform', 'translateX(2.05rem)')
+    themesBtnStyle.style.setProperty('background-image', 'url("../img/Moon-logo.png")')
+    themesBtnBorderStyle.style.setProperty('border', '0.25rem solid white')
+    themesBtnBorderStyle.style.setProperty('background-color', 'grey')
+    cssRules.get('.desktop-header').style.setProperty('background-color', 'rgb(0, 0, 50)')
+    if (state === 'change') {
+        cssRules.get('.desktop-header').style.setProperty('transition', 'background-color 0.5s ease-in-out')
+        setTimeout(function () {
+            cssRules.get('.desktop-header').style.setProperty('transition', 'none')
+        }, 500)
     }
-    else
-    {
-        themesBtnState = 'left'
-        localStorage.setItem('themesBtnState', 'left')
-        themesBtnStyle.style.setProperty('transform', 'none')
-        themesBtnStyle.style.setProperty('background-image', 'url("../img/Sun-logo.png")')
-        themesBtnBorderStyle.style.setProperty('border', '0.25rem solid rgb(50, 50, 50)')
-        themesBtnBorderStyle.style.setProperty('background-color', 'white')
-        /*
+}
 
-        Insert Code to change styling to light theme
+if (themesBtnState === null) {
+    lightTheme()
+}
+else {
+    if (themesBtnState === 'right') {
+        darkTheme()
+    }
+    else {
+        lightTheme()
+    }
+}
 
-        */
+themesBtnHtml.addEventListener('click', function () {
+    if (themesBtnState === 'left') {
+        darkTheme('change')
+    }
+    else {
+        lightTheme('change')
     }
 })
