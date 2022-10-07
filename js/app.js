fcfd18b1bbbea7470f59a85896373801bd643b75
Fouad <kazemfouad53@gmail.com>
2022-10-08 00:15:19 +0400

    Modified app.js
    Added ability to detect the user agent of the browser

--- js/app.js
+++ js/app.js
@@ -1,22 +1,30 @@
 const styleSheet = document.styleSheets[0]
 const themesBtnHtml = document.querySelector(".themes-btn-slide")
-let cssRules = new Map()
-let themesBtnStyle = null
-let themesBtnBorderStyle = null
 let themesBtnState = localStorage.getItem('themesBtnState')
+let cssRules = new Map()
 for (let i = 0; i < styleSheet.cssRules.length; i++) {
     cssRules.set(styleSheet.cssRules[i].selectorText, styleSheet.cssRules[i])
 }
-themesBtnStyle = cssRules.get('.themes-btn-slide')
-themesBtnBorderStyle = cssRules.get('.themes-btn')
+console.log(navigator.userAgent)
+let userAgent = navigator.userAgent.toLowerCase()
+let mobileAgent = false
+if (userAgent.indexOf('android') != -1 || userAgent.indexOf('iphone') != -1 || userAgent.indexOf('ipad')) {
+    mobileAgent = true
+}
+if (mobileAgent) {
+    cssRules.get('.desktop-header').style.setProperty('display', 'none')
+}
+else {
+    cssRules.get('.mobile-header').style.setProperty('display', 'none')
+}
 
 function lightTheme(state) {
     themesBtnState = 'left'
     localStorage.setItem('themesBtnState', 'left')
-    themesBtnStyle.style.setProperty('transform', 'none')
-    themesBtnStyle.style.setProperty('background-image', 'url("../img/Sun-logo.png")')
-    themesBtnBorderStyle.style.setProperty('border', '0.25rem solid rgb(50, 50, 50)')
-    themesBtnBorderStyle.style.setProperty('background-color', 'white')
+    cssRules.get('.themes-btn-slide').style.setProperty('transform', 'none')
+    cssRules.get('.themes-btn-slide').style.setProperty('background-image', 'url("../img/Sun-logo.png")')
+    cssRules.get('.themes-btn').style.setProperty('border', '0.25rem solid rgb(50, 50, 50)')
+    cssRules.get('.themes-btn').style.setProperty('background-color', 'white')
     cssRules.get('.desktop-header').style.setProperty('background-color', 'rgb(0, 128, 255)')
     if (state === 'change') {
         cssRules.get('.desktop-header').style.setProperty('transition', 'background-color 0.5s ease-in-out')
@@ -29,10 +37,10 @@ function lightTheme(state) {
 function darkTheme(state) {
     themesBtnState = 'right'
     localStorage.setItem('themesBtnState', 'right')
-    themesBtnStyle.style.setProperty('transform', 'translateX(2.05rem)')
-    themesBtnStyle.style.setProperty('background-image', 'url("../img/Moon-logo.png")')
-    themesBtnBorderStyle.style.setProperty('border', '0.25rem solid white')
-    themesBtnBorderStyle.style.setProperty('background-color', 'grey')
+    cssRules.get('.themes-btn-slide').style.setProperty('transform', 'translateX(2.05rem)')
+    cssRules.get('.themes-btn-slide').style.setProperty('background-image', 'url("../img/Moon-logo.png")')
+    cssRules.get('.themes-btn').style.setProperty('border', '0.25rem solid white')
+    cssRules.get('.themes-btn').style.setProperty('background-color', 'grey')
     cssRules.get('.desktop-header').style.setProperty('background-color', 'rgb(0, 0, 50)')
     if (state === 'change') {
         cssRules.get('.desktop-header').style.setProperty('transition', 'background-color 0.5s ease-in-out')
